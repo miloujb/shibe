e7ab7c059caed241fb3ef5f8dd7b3965af81873d
miloujb <emilyjbailey@hotmail.co.uk>
2020-05-24 12:27:56 +0100

    changed description

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -3,7 +3,7 @@ import React from "react";
 const Description = () => {
   return (
     <div>
-      <h4>So doge, such happy. So button, wow doge.</h4>
+      <h4>So doge, much happy. Very button, wow doge.</h4>
       <p>
         (And if that made no sense, click 'many doge' to see the many doge. Wow.
       </p>
