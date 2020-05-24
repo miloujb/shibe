9093fe6898770e15fa431552c5acc66c50eb9b0c
miloujb <emilyjbailey@hotmail.co.uk>
2020-05-24 12:28:41 +0100

    further description changes

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -5,7 +5,7 @@ const Description = () => {
     <div>
       <h4>So doge, much happy. Very button, wow doge.</h4>
       <p>
-        (And if that made no sense, click 'many doge' to see the many doge. Wow.
+        (And if that made no sense, click 'very doge' to see the very doge. Wow. Such fascinate.
       </p>
     </div>
   );
